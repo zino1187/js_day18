dc80705f09626c74cc7ef68d91bf840e4753eeb2
zino1187 <zino1187@gmail.com>
2018-12-06 15:19:34 +0900

    no message

--- Cell2.js
+++ Cell2.js
@@ -24,7 +24,7 @@ class Cell{
 		this.div.style.height=this.height+"px";
 		this.div.style.background=this.bg;
 		this.div.style.border=1+"px solid #000";
-		this.div.innerText=this.text;
+		this.div.innerHTML=this.text;
 		this.div.style.fontSize=20+"px";
 		this.div.style.float="left";
 
